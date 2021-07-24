fed71ce07ebe8e316fb2dbbe5baf9057931920a5
cuiwenyao <yao1970099540@163.com>
2021-07-24 21:21:39 +0800

    第一版

---
+++ trojan-install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+function blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+function green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+function red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+function version_lt(){
+    test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; 
+}
+
+green "安装trojan"
+# 安装trojan 要求 Ubuntu 20.04 以上
+apt -y install trojan
+
+read -p "请输入你的域名 :" trojan_domain
+
+read -p "请输入密码 :" trojan_passwd
+
+# 写入配置文件
+    cat > config.json <<-EOF
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "$trojan_domain",
+    "remote_port": 80,
+    "password": [
+        "$trojan_passwd"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/etc/trojan/trojancert/${trojan_domain}.crt",
+        "key": "/etc/trojan/trojancert/${trojan_domain}.key",
+        "key_password": "",
+        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": ""
+    }
+}
+EOF
+
+green "trojan设为自启动"
+systemctl enable trojan
+
+green "restart trojan"
+systemctl restart trojan
+
+green "trojan安装成功"
+
+
+
